a743fe6d484b2901f7144b24414d3ffe6de1f7ab
mahir-d <mahirdhall4@gmail.com>
2020-11-19 14:20:41 -0500

    back-end routes complete

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const { ApolloServer, gql } = require('apollo-server');
 const axios = require('axios')
 const { v4: uuidv4, stringify } = require('uuid');
-const redis = require("redis");
+
+const bluebird = require('bluebird');
+const redis = require('redis');
 const client = redis.createClient();
-const asyncRedis = require("async-redis");
-const asyncRedisClient = asyncRedis.decorate(client);
+
+bluebird.promisifyAll(redis.RedisClient.prototype);
+bluebird.promisifyAll(redis.Multi.prototype);
+
 
 client.on('connect', function () {
     console.log('connected');
@@ -14,10 +18,14 @@ const typeDefs = gql`
   type Query {
     unSplashImages(pageNum: Int!): [ImagePost]
     binnedImages: [ImagePost]
+    userPostedImages: [ImagePost]
   }
 
   type Mutation{
     uploadImage(url: String!, description: String, posterName: String): ImagePost
+    updateImage(id: ID!, url: String, posterName: String, description: String, 
+        userPosted: Boolean, binned: Boolean) : ImagePost
+    deleteImage(id: ID!): ImagePost
   }
 
   type ImagePost {
@@ -30,6 +38,36 @@ const typeDefs = gql`
 }
 `;
 
+async function getLen() {
+
+    let redis_len = await client.LLENAsync("Image_List")
+    return redis_len
+
+}
+async function getImages() {
+    let redis_len = await getLen()
+    image_list = await client.LRANGEAsync("Image_List", 0, redis_len)
+    return image_list
+}
+
+async function removeImage(index, image) {
+    let image_obj = await client.LREMAsync("Image_List", index, image)
+    return image_obj
+}
+
+
+function uploadImageToCache(args) {
+    var image = {
+        "id": args.id,
+        "url": args.url,
+        "posterName": args.posterName,
+        "description": args.description,
+        "userPosted": true,
+        "binned": false
+    }
+    client.rpush("Image_List", JSON.stringify(image))
+    return image
+}
 
 
 
@@ -51,45 +89,35 @@ const resolvers = {
                 return image
             })
         },
-        binnedImages() {
-            function callLLen() {
-                client.LLEN("Image_List", function (err, reply) {
-
-                    return reply
-
-                })
-            }
-
-            function rtrn_data(i, binned_images) {
-                client.lindex("Image_List", i, function (err, reply) {
-                    js_obj = JSON.parse(reply)
-                    console.log(`json = ${js_obj.binned}`)
-                    if (js_obj.binned) {
-                        binned_images.push({ "id": js_obj.id, "url": js_obj.url, "posterName": js_obj.posterName, "description": js_obj.description, "userPosted": js_obj.userPosted, "binned": js_obj.binned })
-
-                    }
-                    return binned_images
-                })
-            }
-
-            function callIndex() {
-                var binned_images = []
-                console.log(`len = ${callLLen()}`)
-                for (i = 0; i < callLLen(); i++) {
-                    binned_images = rtrn_data(i, binned_images)
+        async binnedImages() {
+            let redis_len = await getLen()
+            image_list = await client.LRANGEAsync("Image_List", 0, redis_len)
+            let binned_images = []
+            image_list.map(image_obj => {
+                js_obj = JSON.parse(image_obj)
+                if (js_obj.binned == true) {
+                    binned_images.push(js_obj)
                 }
+            })
+            return binned_images
+        },
+        async userPostedImages() {
 
-                return binned_images
-            }
-
-            return callIndex()
-
+            image_list = await getImages()
+            let binned_images = []
+            image_list.map(image_obj => {
+                js_obj = JSON.parse(image_obj)
+                if (js_obj.userPosted == true) {
+                    binned_images.push(js_obj)
+                }
+            })
+            return binned_images
 
         }
     },
     Mutation: {
         async uploadImage(parent, args, context, info) {
-            console.log(args)
+
             uui = uuidv4()
             var image = {
                 "id": uui,
@@ -97,14 +125,58 @@ const resolvers = {
                 "posterName": args.posterName,
                 "description": args.description,
                 "userPosted": true,
-                "binned": true
+                "binned": false
             }
-            u_id = uui.toString()
+
 
             client.rpush("Image_List", JSON.stringify(image))
 
 
             return image
+        },
+        async updateImage(parent, args, context, info) {
+            let image_list = await getImages()
+            let curr_image = undefined
+            image_list.map(image => {
+                image = JSON.parse(image)
+                if (image.id == args.id) {
+
+                    curr_image = image
+
+                }
+
+            })
+
+            if (curr_image === undefined) {
+                let image = uploadImageToCache(args)
+                return image
+            }
+            else {
+                removeImage(0, JSON.stringify(curr_image))
+                let image = uploadImageToCache(args)
+                return image
+
+            }
+        },
+        async deleteImage(parent, args, context, info) {
+            let image_list = await getImages()
+            let index = 0
+            let curr_image = null
+            image_list.map(image => {
+                image = JSON.parse(image)
+                if (image.id == args.id) {
+
+                    curr_image = image
+
+                }
+                index++
+            })
+            if (curr_image) {
+                let image_obj = await removeImage(0, JSON.stringify(curr_image))
+                return image_obj
+            }
+
+            return curr_image
         }
     }
 };
